15a964bbd479ab66b015e8029a5191f8ef35e4d3
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2020-07-14 23:26:40 +0100

    Extend n-gram analysis: most-common n-grams w/ iterative removal

--- analysis/basic-ngram-counts.py
+++ analysis/basic-ngram-counts.py
@@ -5,26 +5,17 @@ from textblob import TextBlob
 from textblob import formats
 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
 
 
 cutoff_total = 15
-ngram_n_max_interesting = 15  # at this length, mostly get count of 1  ###
-
-
-class NewLineDelimitedFormat(formats.DelimitedFormat):
-    ''' Allow names separated by newlines as sentence boundary markers.
-
-    By default the textblob library uses dots as means for sentence boundary
-    disambiguation, given its NLP motivation.
-    '''
-    delimiter = '\n'
+ngram_n_max_interesting = 20  # at this length, mostly get count of 1
 
 
 def get_ngram_counts(names, ngram_size, get_x_most_common=None):
     '''TODO
     '''
-
-    # Note: can't simply do:
+    # Note, can't simply do:
     # ngram = names.ngrams(ngram_size)
     # as it takes ngrams across sentences, i.e. start & end of names in order.
     names_sentences = names.sentences
@@ -46,7 +37,83 @@ def get_ngram_counts(names, ngram_size, get_x_most_common=None):
     return ngram_counts
 
 
-def plot_nrgam_counts(ngram_counts, cutoff_number, ngram_n):
+def get_and_remove_any_n_ngram_counts(names):
+    '''TODO
+    '''
+    most_common_any_n_ngrams = []
+    max_count = 0
+    # This time remove ngrams as they are found!
+    names_sentences = names.sentences
+    # reversed(...) is important here.
+    for size in reversed(range(2, ngram_n_max_interesting)):
+        # Get n-grams at this size
+        size_n_ngrams = []
+        for name in names_sentences:
+            ngrams_of_size = name.ngrams(size)
+            size_n_ngrams.extend(ngrams_of_size)
+        most_common_size_n = Counter(
+            [tuple(wordlist) for wordlist in size_n_ngrams]).most_common(1)
+
+        # TODO: ensure nothing draws, as will need to add both!
+        ngram_max_count = most_common_size_n[0][1]
+
+        if ngram_max_count >= max_count:  # take longer one
+            most_common_any_n_ngrams.append(most_common_size_n)
+            max_count = ngram_max_count
+
+    # Determine most common over any size:
+    final_most_common_any_n_ngrams = sorted(
+        most_common_any_n_ngrams, key=lambda item: item[0][1], reverse=True)
+    # TODO: manage this mess; need to catch counts of equal value post-sort
+    if (final_most_common_any_n_ngrams[0][0][1] ==
+            final_most_common_any_n_ngrams[1][0][1]):
+        print("\nNote, duplicates:", final_most_common_any_n_ngrams[:2])
+        if (final_most_common_any_n_ngrams[0][0][0][:-1] ==
+                final_most_common_any_n_ngrams[1][0][0]):
+            pass
+        if (final_most_common_any_n_ngrams[1][0][1] ==
+                final_most_common_any_n_ngrams[2][0][1]):
+            print("\nFURTHER DUPLICATES, CHECK HOW FAR EQUAL MAX. GOES...",
+                  final_most_common_any_n_ngrams)
+
+    return final_most_common_any_n_ngrams[0], names  # any_n_ngram_counts
+
+
+def recursive_get_and_remove_any_n_ngram_counts(names, get_x_most_common):
+    '''TODO
+    '''
+    # Get most common n-gram of any size:
+    overall_most_common_ngrams = []
+    use_names = names
+    while len(overall_most_common_ngrams) < get_x_most_common:
+        packed_most_freq_ngram, all_names = get_and_remove_any_n_ngram_counts(
+            use_names)
+        most_common_n_gram = packed_most_freq_ngram
+        overall_most_common_ngrams.append(most_common_n_gram)
+
+        # Remove that most common n-gram from all names ready to go again:
+        most_common_ngram_raw = " ".join(most_common_n_gram[0][0])
+        new_names = []
+        for name in all_names.sentences:
+            if most_common_ngram_raw in name:
+                name = name.replace(most_common_ngram_raw, "")
+                name = name.replace("  ", " ")
+            new_names.append(name)
+        new_names = "\n".join([str(name) for name in new_names])
+        use_names = TextBlob(new_names)
+
+    # Format to get phrase instead of tuple of words as keys:
+    ngram_counts = {
+        entry[0][0]: entry[0][1] for entry in overall_most_common_ngrams}
+    ngram_counts = {
+        " ".join(phrase): count for phrase, count in ngram_counts.items()}
+    print(ngram_counts)
+    return ngram_counts
+
+
+def plot_ngram_counts(ngram_counts, cutoff_number=None, ngram_n=None):
+    '''TODO
+    '''
     plt.rcdefaults()
     fig, ax = plt.subplots()
     names = list(ngram_counts.keys())
@@ -78,17 +145,51 @@ def plot_nrgam_counts(ngram_counts, cutoff_number, ngram_n):
     plt.show()
 
 
+def plot_recursive_ngram_counts(ngram_counts):
+    '''TODO
+    '''
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+    names = list(ngram_counts.keys())
+    counts = list(ngram_counts.values())
+    ax.barh(names, counts, color='lightcoral')
+
+    # Axes formatting:
+    ax.set_xscale('log')
+    ax.xaxis.set_major_formatter(ScalarFormatter())
+    ax.set_yticklabels(names)
+    ax.tick_params(axis="y", direction="in", pad=-10)
+    plt.setp(ax.get_yticklabels(), ha="left")
+    for i, (name, count) in enumerate(ngram_counts.items()):
+        label = "{} ({})".format(name, count)
+        plt.text(
+            s=label, x=count, y=i,
+            color="black", verticalalignment="center",
+            ha="left", size=10,
+        )
+    plt.yticks([])
+
+    # Labelling:
+    ax.set_title(
+        '{} most common n-grams of *any* size for the '
+        'CF Standard Names'.format(len(names))
+    )
+    ax.set_xlabel('Frequency across all standard names in current table')
+
+    fig.set_size_inches(12, 12)
+    plt.savefig(
+        "../results/ngrams/recursive-n-grams-of-any-n.png", dpi=400)
+    plt.show()
+
+
 # Grab all of the newline-delimited names (with underscores replaced by spaces)
 with open("../data/all-raw-names-10.07.20-gen.txt", "r") as all_raw_names:
     names = all_raw_names.readlines()
 
 
-# Replace newline with dot for standard NLP sentence delimiter, to simplify.
-# TODO: use NewLineDelimitedFormat rather than manual join... not obvious how?
-# formats.register('nd', NewLineDelimitedFormat)
+# Replace newline with dot for standard NLP sentence delimiter, to simplify:
 names = ". ".join(names)
 names = names.replace("\n", "")
-
 # Create the textblob core object, a TextBlob, of all the names:
 names_object = TextBlob(names)
 
@@ -102,4 +203,8 @@ for ngram_size in range(2, ngram_n_max_interesting + 1):
     except KeyError:
         pass  # no such key (good/correct)
 
-    plot_nrgam_counts(counts, cutoff_total, ngram_size)
+    plot_ngram_counts(counts, cutoff_total, ngram_size)
+
+# Find, plot and save the 50 most common ngrams of any size, with removal:
+plot_recursive_ngram_counts(
+    recursive_get_and_remove_any_n_ngram_counts(names_object, 50))
--- summaries/most-common-ngrams.md
+++ summaries/most-common-ngrams.md
@@ -5,11 +5,13 @@
 An [*n*-gram](http://www.cs.columbia.edu/~kathy/NLP/ClassSlides/Class3-ngrams09/ngrams.pdf) is a sequence of length *n* of tokens (linguistic units). Here we
 take single words as the tokens to analyse.
 
-Note here we ignore 1-grams (*n*-grams with *n = 1*, i.e. single words) as these
-will be analysed in detail separately.
+Note here we ignore 1-grams (*n*-grams with *n = 1*, i.e. single words) as
+these will be analysed in detail separately.
 
 
 ## Results
+
+### *n*-grams of set *n* without removal
 
 The most frequent *n*-grams of words in the full current (10/07/20) Standard
 Names table of names for *n = 2* to *n = 15* words, are as follows (note the
@@ -17,74 +19,125 @@ underscores in names have been replaced by whitespace to mimic natural text
 for enhanced readability):
 
 
-### Bigrams (2-grams)
+#### Bigrams (2-grams)
 
 ![2-gram](../results/ngrams/most-common-2-grams.png)
 
 
-### Trigrams (3-grams)
+#### Trigrams (3-grams)
 
 ![3-gram](../results/ngrams/most-common-3-grams.png)
 
 
-### 4-grams
+#### 4-grams
 
 ![4-gram](../results/ngrams/most-common-4-grams.png)
 
 
-### 5-grams
+#### 5-grams
 
 ![5-gram](../results/ngrams/most-common-5-grams.png)
 
 
-### 6-grams
+#### 6-grams
 
 ![6-gram](../results/ngrams/most-common-6-grams.png)
 
 
-### 7-grams
+#### 7-grams
 
 ![7-gram](../results/ngrams/most-common-7-grams.png)
 
 
-### 8-grams
+#### 8-grams
 
 ![8-gram](../results/ngrams/most-common-8-grams.png)
 
 
-### 9-grams
+#### 9-grams
 
 ![9-gram](../results/ngrams/most-common-9-grams.png)
 
 
-### 10-grams
+#### 10-grams
 
 ![10-gram](../results/ngrams/most-common-10-grams.png)
 
 
-### 11-grams
+#### 11-grams
 
 ![11-gram](../results/ngrams/most-common-11-grams.png)
 
 
-### 12-grams
+#### 12-grams
 
 ![12-gram](../results/ngrams/most-common-12-grams.png)
 
 
-### 13-grams
+#### 13-grams
 
 ![13-gram](../results/ngrams/most-common-13-grams.png)
 
 
-### 14-grams
+#### 14-grams
 
 ![14-gram](../results/ngrams/most-common-14-grams.png)
 
 
-### 15-grams
+#### 15-grams
 
 ![15-gram](../results/ngrams/most-common-15-grams.png)
+
+
+#### 16-grams
+
+![16-gram](../results/ngrams/most-common-16-grams.png)
+
+
+#### 17-grams
+
+![17-gram](../results/ngrams/most-common-17-grams.png)
+
+
+#### 18-grams
+
+![18-gram](../results/ngrams/most-common-18-grams.png)
+
+
+#### 19-grams
+
+![19-gram](../results/ngrams/most-common-19-grams.png)
+
+
+#### 20-grams
+
+![20-gram](../results/ngrams/most-common-20-grams.png)
+
+
+#### *n*-grams of higher *n*
+
+After *n*=20 there are no more *n*-grams that occur more than once across
+all of the names. Hence further plots are not of relevance.
+
+
+### *n*-grams of any *n* with recursive removal
+
+This time, consider *n*-grams of any length and follow a process of
+iterative removal, as follows:
+
+1. find the most common *n*-gram of any length for the names, where a longer
+   *n*-gram is preferred (i.e. chosen over) to a shorter one with equal count,
+   since any *n*-gram contained in a larger *n+N*-gram (for integer `N >= 1`)
+   will necessarily have at minimum the same count as the *n+N*-gram.
+2. remove this most-common *n*-gram from all names;
+3. go back to step (1), taking the names reduced under (2) as the new input
+   names;
+4. continue this process until the *X* most common *n*-grams of any length,
+   excluding previous most-common *n*-grams, are found.
+
+The results for the most common `X=50` are as follows:
+
+![any-n-gram](../results/ngrams/recursive-n-grams-of-any-n.png)
 
 
 *****
