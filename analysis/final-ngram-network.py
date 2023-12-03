c087f9e18d548d6ae5950d04d9635be460f5ff2f
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2023-12-03 19:09:36 +0000

    Update digraph output: create shells for shell layout

--- analysis/final-ngram-network.py
+++ analysis/final-ngram-network.py
@@ -15,7 +15,7 @@ SHORT_CIRCUIT_TO_N_NAMES = False  #1000  # int to short circuit, or False to not
 
 # Note: ~21,000 nodes for the 2 cutoff full-dataset graph! Graphs are big!
 # CUTOFF OF 3 OR LESS IS TOO MEMORY-INTENSIVE! NEED TO USE JOB(?) ARCHER?
-FREQUENCY_CUTOFF = 250  # v >= FREQUENCY_CUTOFF for inclusion
+FREQUENCY_CUTOFF = 15  # v >= FREQUENCY_CUTOFF for inclusion
 # samples to do: 1, 2, 3, 5, 10, 15, 25, 50, 100, 250, 500
 # NEW ONES: 300 - for spiral graph to add?
 
@@ -33,7 +33,7 @@ USE_PREV_DATA = (
 
 
 # Quick graph customisation
-LABELS_ON = True
+LABELS_ON = False
 LABEL_OFFSET = 0.01  # 0.02-0.05 is good
 
 # Use rainbow colours - need lack of white tones or repeated tones at each
@@ -144,7 +144,7 @@ def get_ngram_counts(names, ngram_size, cutoff_freq=1):
     return ngram_counts
 
 
-def get_all_ngram_counts(init_data):
+def get_all_ngram_counts(init_data, range_n):
     """TODO."""
     space_and_dot_delim_data = ". ".join(init_data)
     data = space_and_dot_delim_data.replace("\n", "")
@@ -324,7 +324,8 @@ def post_processing_of_graph(graph, layout):
             t.set_rotation_mode("anchor")
 
 
-def draw_graph_with_layout(graph, node_sizes, node_colours, edge_colours):
+def draw_graph_with_layout(
+        graph, node_sizes, node_colours, edge_colours, shells):
     """Apply a layout and customisations to define how to draw the graph."""
     options = {
         ###"node_color": "tab:red",
@@ -356,7 +357,7 @@ def draw_graph_with_layout(graph, node_sizes, node_colours, edge_colours):
     ###layout = nx.nx_agraph.graphviz_layout(graph, prog="twopi", root=0)
 
     # shell layout does concentric circles!
-    layout = nx.shell_layout(graph)  ###,scale=0.4)
+    layout = nx.shell_layout(graph, nlist=shells)  ###,scale=0.4)
     ###layout = nx.kamada_kawai_layout(graph)
 
     nx.draw(
@@ -367,6 +368,27 @@ def draw_graph_with_layout(graph, node_sizes, node_colours, edge_colours):
     )
 
     return graph, layout
+
+
+def generate_shells(nodes):
+    """TODO."""
+    # DEFINE SHELLS! - shells are to be arranged according to n-gram word size
+    # whic is already identified as the node_colours
+    nodes_list_per_n = []
+    for i in range(1, 25):  # TODO UPDATE 1, 25 TO GET REL. RANGE NOT JUST MANUAL
+        i_shell = []
+        for id_n, n in nodes.items():
+            ngram, _ = n
+            # TODO make helper function for this calc, do often
+            if ngram.count(" ") + 1 == i:  # i.e. length
+                if LABELS_ON:
+                    i_shell.append(ngram)
+                else:
+                    i_shell.append(id_n)
+
+        nodes_list_per_n.append(i_shell)
+
+    return nodes_list_per_n
 
 
 def create_sn_nrgam_graph(nodes, edges):
@@ -408,10 +430,11 @@ def create_sn_nrgam_graph(nodes, edges):
     ### print("EDGE COLOURS ARE:", edge_colours)
 
     # N-2. <Numbering>
+    shells = generate_shells(nodes)
 
     # N-1. Customise the layout to use and plot the graph using it
     G, layout = draw_graph_with_layout(
-        G, node_sizes, node_colours, edge_colours)
+        G, node_sizes, node_colours, edge_colours, shells)
 
     # N. Apply post-processing, e.g. to reposition node labels based on layout
     post_processing_of_graph(G, layout)
@@ -427,6 +450,7 @@ def create_sn_nrgam_graph(nodes, edges):
 if __name__ == "__main__":
     # 0. Define data
     # OR (ALTNERATIVE LATER) 3. Load from file to save re-generating
+    
     if USE_PREV_DATA:
         with open(USE_PREV_DATA, "r") as data_input_file:
             all_ngram_data = json.load(data_input_file)
@@ -441,7 +465,8 @@ if __name__ == "__main__":
 
         # 3. Find all n-grams occuring more than the cutoff amount across the
         #    names for all given n in the word_length_ranges range
-        all_ngram_data = get_all_ngram_counts(orig_data)
+        all_ngram_data = get_all_ngram_counts(orig_data, range_n)
+
 
     print("NGRAM DATA STRUCTURE CALC'ED IS:")
     pprint(all_ngram_data)
